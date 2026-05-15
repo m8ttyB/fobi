f3de9b73c5ff22cbb44d5eacf559c4c5bdba4262
Matt Brandt <matt@secretmustache.com>
2026-05-15 16:53:29 -0600

    config: update model to m8ttyb/gemma-4-E4B-it-4bit

--- hf/writing-assistant/config.py
+++ hf/writing-assistant/config.py
@@ -3,7 +3,8 @@ import os
 
 MODEL_PATH = os.environ.get(
     "WA_MODEL_PATH",
-    "mlx-community/gemma-3-4b-it-4bit",
+    # "mlx-community/gemma-3-4b-it-4bit",
+    "m8ttyb/gemma-4-E4B-it-4bit",
 )
 HOST = os.environ.get("WA_HOST", "127.0.0.1")
 PORT = int(os.environ.get("WA_PORT", "8000"))
